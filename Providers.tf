a11da9026ba655df732970ff65fb6484c1b25ade
Raaj2689 <raajkhamal@gmail.com>
2025-06-15 20:19:30 +0530

    Update Providers.tf
    slight chnages made

--- Providers.tf
+++ Providers.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-source  = "hashicorp/azurerm"
-version = "=3.0.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
 }
-#Configure the Microsoft Azure Provider
 provider "azurerm" {
-    subscription_id = "017b3490-6be9-49a4-ba0d-795637b2bce2"
-    tenant_id = "fd73ca01-1377-4863-ad01-5e76213c50e1"
-  features {}
+  subscription_id = "017b3490-6be9-49a4-ba0d-795637b2bce2"
+  tenant_id       = "fd73ca01-1377-4863-ad01-5e76213c50e1"
+  features        {}
 }
