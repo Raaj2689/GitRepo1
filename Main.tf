fe236bc82506a8677e428bea94c46bd9685235d3
Raaj2689 <raajkhamal@gmail.com>
2025-06-15 19:56:12 +0530

    Add files via upload

---
+++ Main.tf
@@ -0,0 +1,94 @@
+resource "azurerm_resource_group" "RG" {
+  name     = "RG_Group"
+  location = var.location
+}
+resource "azurerm_public_ip" "pub001" {
+  name                = "TestPublicIP"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.RG.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+resource "azurerm_virtual_network" "AVN" {
+  name                = "Testnetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "Testinternal"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.AVN.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "test_network" {
+  name                = "LAN-Test"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id        = azurerm_public_ip.pub001.id
+  }
+}
+
+resource "azurerm_network_security_group" "test_NSG" {
+  name                = "TestSecurityGroup1"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "NSG_Association" {
+  network_interface_id      = azurerm_network_interface.test_network.id
+  network_security_group_id = azurerm_network_security_group.test_NSG.id
+}
+
+resource "azurerm_windows_virtual_machine" "TerraformVM" {
+  name                  = "TestVM"
+  resource_group_name   = azurerm_resource_group.RG.name
+  location              = var.location
+  size                  = "Standard_F2"
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+  network_interface_ids = [azurerm_network_interface.test_network.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  tags = {
+    Application      = "MSoffice"
+    Audit_Scope      = "No"
+    Environment      = "Test"
+    Owner            = "mahesh"
+    Horizontal       = "A"
+    Vertical         = "B"
+    Tier             = "Data"
+    Technical_Owner  = "Leo"
+  }
+}
